085c5fdaacbc9367c1078462a7b12ac5ea526b31
quinhos <marco.castagnaro@ing.austral.edu.ar>
2024-06-29 11:58:53 -0300

    change url

--- src/utils/mock/fakeSnippetOperations.ts
+++ src/utils/mock/fakeSnippetOperations.ts
@@ -16,7 +16,7 @@ const token = localStorage.getItem('token')
 const userId = localStorage.getItem('userId')
 
 //use localhost
-const url =  `${window.location.origin}/operations`
+const url =  `${window.location.origin}/api/operations`
 const SNIPPET_URL = `${url}/snippets`
 const TEST_CASE_URL = `${url}/test-case`
 const RUN_URL = `${url}/run`
